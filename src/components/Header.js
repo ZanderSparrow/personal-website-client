b94e88bd75343248606982ad04e76895ed927fd4
Alex Sparrow <alexsilverhawk@gmail.com>
2021-09-13 14:03:30 -0700

    Align header text center

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <h1 className="header__primary">Alexander Sparrow</h1>
+      <h3 className="header__seconday">Fullstack Software Developer</h3>
+    </div>
+  );
 };
 
 export default Header;
